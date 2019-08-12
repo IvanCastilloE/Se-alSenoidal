faa664709343fd24d1bdbb9b7eaaed1a355aa98e
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-12 11:49:46 -0700

    Agregar archivos de proyecto.

---
+++ SeñalSenoidal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeñalSenoidal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SeñalSenoidal señalSenoidal;
+            señalSenoidal = new SeñalSenoidal();
+            Console.WriteLine(señalSenoidal.evaluar(0));
+            Console.WriteLine(señalSenoidal.evaluar(0.05));
+            Console.WriteLine(señalSenoidal.evaluar(0.1));
+            Console.WriteLine(señalSenoidal.evaluar(0.15));
+            Console.WriteLine(señalSenoidal.evaluar(0.2));
+            Console.WriteLine(señalSenoidal.evaluar(0.25));
+            Console.WriteLine(señalSenoidal.evaluar(0.3));
+            Console.WriteLine(señalSenoidal.evaluar(0.35));
+            Console.WriteLine(señalSenoidal.evaluar(0.4));
+            Console.WriteLine(señalSenoidal.evaluar(0.45));
+            Console.WriteLine(señalSenoidal.evaluar(0.5));
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ SeñalSenoidal/SeñalSenoidal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeñalSenoidal
+{
+    class SeñalSenoidal
+    {
+        //Auto Properties
+        //Atributos encapsulados de c#
+        //no tienen rutinas al establecer u obtener valor
+        public double Amplitud { get; set; }
+        public double Fase { get; set; }
+        public double Frecuencia { get; set; }
+
+        //Constructor se ejecuta al instanciar la clase, sintaxis: public NombreDeLaClase(parametros)
+        public SeñalSenoidal()
+        {
+            Amplitud = 1.0;
+            Fase = 0.0;
+            Frecuencia = 1.0;
+        }
+        public SeñalSenoidal(double amplitud,double fase,double frecuencia)
+        {
+            Amplitud = amplitud;
+            Fase = fase;
+            Frecuencia = frecuencia;
+        }
+        public double evaluar(double tiempo)
+        {
+            double resultado = 0.0;
+            //Evaluar la función
+            resultado = Amplitud * (Math.Sin((2.0 * Math.PI * Frecuencia * tiempo) + Fase));
+
+            return resultado;
+        }
+    }
+}
